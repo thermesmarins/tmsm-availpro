63edf50072ef46eb028c3252d7bcb2bf4133c724
aflamentTM <aflament@thalasso-saintmalo.com>
2024-08-09 15:37:09 +0200

    refactoring webservice, webservice-oauth, and error response from api

--- includes/class-tmsm-availpro-webservice-oauth.php
+++ includes/class-tmsm-availpro-webservice-oauth.php
@@ -53,7 +53,6 @@ public static function buildAuthorizationHeader($oauth) {
     return $r;
 }
  
-// public function getOauth1Header($url, $method, $consumerKey, $consumerSecret, $accessToken, $accessSecret, $jsonBody) {
 public static function getOauth1Header($url, $method, $oauth_identifiers) {
     $oauth = array(
         'oauth_consumer_key' => $oauth_identifiers['consumerKey'],
@@ -72,49 +71,8 @@ public static function getOauth1Header($url, $method, $oauth_identifiers) {
     return self::buildAuthorizationHeader($oauth);
 }
 public function getSignature($url, $method) {
-    // $method = "POST";
-
     $oauth_identifiers = $this->oauth_identifiers;
     $authHeader = self::getOauth1Header($url, $method, $oauth_identifiers);
     return $authHeader;
 }
-// Préparation des paramètres
-// $url = "https://ws.availpro.com/planning/2018A/Planning/GetDailyPlanning";
-// $method = "POST";
-// $consumerKey = "ThermesMarinsWS";
-// $consumerSecret = "1ZVqDEbCWGgKpKj9QmuwLw";
-// $accessToken = "1B6L5D7BH7E8GG6VFLUJRS54V";
-// $accessSecret = "V8gny863J6J4oXPP7pzLSA";
-// $jsonBody = '{"beginDate": "2024-12-12","endDate": "2024-12-20","filter": {"ratePlans": [{"hotels": {"exceptions": ["6286"],"default": "Excluded"},"groupId": "5692","referenceRateCode": ""}],"status": ["Available","NotAvailable"]},"layout": {"levels": [{"name": "ArticleRate","properties": ["Availability","Status","Price","TotalPrice","CrossedOutPrice","MaximumPrice","MinimumPrice","DiscountPrice","DiscountNightCount","PublicOffer","MixedRates","IncludedArticles","Occupancies","OriginalPrice","Taxes","MaximumStayOnDeparture","MaximumStayThrough","MaximumStayOnArrival","MinimumStayOnDeparture","MinimumStayThrough","MinimumStayOnArrival","ClosedOnDeparture","ClosedOnArrival"]}]},"impersonation": {"hotelId": "6286","groupId": "5692"}}';
-// récupère le fichier js pour que ce soit plus facile pour les variables dont j'ai besoin
-// $jsonBody = file_get_contents('../public/assets/app.js', false, null,11 );
-// echo print_r($jsonBody, true);
-// Transforme le fichier js en objet php
-// $body = json_decode($jsonBody);
-// $authHeader = getOauth1Header($url, $method, $consumerKey, $consumerSecret, $accessToken, $accessSecret, $jsonBody);
-// $authHeader = getOauth1Header($url, $method, $this->oauth_identifiers);
- 
-// Préparation de la requete
-// $options = array(
-//     'http' => array(
-//         'header'  => $authHeader . "\r\n" .
-//                      "Content-Type: application/json\r\n",
-//         'method'  => $method,
-//         'content' => $jsonBody,
-//     ),
-// );
-// $context  = stream_context_create($options);
- 
-// $result = file_get_contents($url, false, $context);
- 
-// if ($result === FALSE) {
-//     echo "Error: " . error_get_last()['message'] . "\n";
-// } else {
-//     echo '<pre>'; 
-//     echo print_r(json_decode($result), true);
-//     echo '</pre>';
-// }
-// die;
-// $this->render('d-edge');
-
 }
--- includes/class-tmsm-availpro-webservice.php
+++ includes/class-tmsm-availpro-webservice.php
@@ -12,58 +12,14 @@
 class Tmsm_Availpro_Webservice {
 
 	/**
-	 * Webservice Namespace
-	 *
-	 * @access 	const
-	 * @since 	1.0.0
-	 * @var 	string
-	 */
-	// const WSNAMESPACE = 'http://ws.availpro.com/schemas/planning/2012A';
-
-	/**
 	 * Webservice URL
 	 *
 	 * @access 	private
 	 * @since 	1.0.0
 	 * @var 	string
 	 */
-	// const URL = 'https://ws.availpro.com/Planning/2012A/PlanningService.asmx?WSDL';
-	//TODO json
 	const URL = 'https://ws.availpro.com/planning/2018A/Planning/';
 
-	/**
-	 * Webservice Oauth identifiers
-	 *
-	 * @access 	private
-	 * @since 	1.0.0
-	 * @var 	array
-	 */
-	// private $oauth_identifiers = [];
-
-	// /**
-	//  * Constructor
-	//  */
-	// public function __construct() {
-
-	// 	$options = get_option('tmsm-availpro-options', false);
-
-	// 	$this->set_oauth_identifiers();
-
-	// }
-
-	// /**
-	//  * Set oauth identifiers
-	//  */
-	// private function set_oauth_identifiers(){
-	// 	$options = get_option('tmsm-availpro-options', false);
-	// 	$this->oauth_identifiers = [
-	// 		'consumerKey'    => $options['consumerkey'],
-	// 		'consumerSecret' => $options['consumersecret'],
-	// 		'accessToken'    => $options['accesstoken'],
-	// 		'accessSecret'   => $options['tokensecret'],
-	// 	];
-	// }
-//TODO json
 	/**
 	 * Get Layout
 	 *
@@ -87,10 +43,6 @@ class Tmsm_Availpro_Webservice {
 		return $layout;
 
 	}
-	// private function get_layout(){
-	// 	return '<level name="ArticleRate"><property name="Status" /><property name="Price" /><property name="Availability" /><property name="MinimumStayThrough" /></level>';
-
-	// }
 	/**
 	 * Get Filters
 	 *
@@ -117,14 +69,7 @@ class Tmsm_Availpro_Webservice {
 		}
 		//TODO json
 		$filters_rateids = [];
-		// $filters_rateids = '';
 		if(!empty($option_rateids_array) && is_array($option_rateids_array) && count($option_rateids_array) > 0){
-			// $filters_rateids = '<rates default="Excluded">';
-			// foreach($option_rateids_array as $item){
-			// 	$filters_rateids .= '<exception id="'.$item.'"/>';
-			// }
-			// $filters_rateids .= '</rates>';
-			//TODO json
 			$filters_rateids['rates'] =[ 				
 					'default'=>'Excluded'
 			];
@@ -144,11 +89,6 @@ class Tmsm_Availpro_Webservice {
 		}
 		$filters_roomids = '';
 		if(!empty($option_roomids_array) && is_array($option_roomids_array) && count($option_roomids_array) > 0){
-			// $filters_roomids = '<rooms default="Excluded">';
-			// foreach($option_roomids_array as $item){
-			// 	$filters_roomids .= '<exception id="'.$item.'"/>';
-			// }
-			// $filters_roomids .= '</rooms>';
 			//TODO json
 			$filters_roomids = array( 
 				'rooms' => array( 
@@ -163,21 +103,8 @@ class Tmsm_Availpro_Webservice {
 		// ratecode
 		$filters_ratecode='';
 		if(!empty($option_ratecode)){
-			// $filters_ratecode = 'referenceRateCode="'.$option_ratecode.'"';
 			$filters_ratecode = $option_ratecode;
 		}
-
-		// @TODO include $filters_roomids but it doesn't give any result with it
-		// @TODO not hardcode OTABAR
-		//referenceRateCode="BARPROM"
-		// $filters = '
-		// 			<ratePlans><ratePlan groupId="'.$option_groupid.'" '.$filters_ratecode.'><hotels default="Excluded"><exception id="'.$option_hotelid.'" /></hotels></ratePlan></ratePlans>'.
-	    //             $filters_rateids.
-	    //             //$filters_roomids.
-	    //             '<currencies default="Excluded"><exception currency="EUR"/></currencies>'.
-	    //             '<status><include status="Available" /><include status="NotAvailable" /></status>'.
-		// '';
-		//TODO json
 		$filters = 
 			array( 
 			'ratePlans'=> array([ 
@@ -226,7 +153,6 @@ class Tmsm_Availpro_Webservice {
 		$month_firstday->modify('first day of this month');
 		$month_lastday = clone $month_firstday;
 		$month_lastday->modify('last day of this month')->modify('+1 day');
-		//$month_lastday->modify('first day of this month')->modify('+6 days');
 
 		if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
 			error_log('get_data');
@@ -234,22 +160,15 @@ class Tmsm_Availpro_Webservice {
 			error_log('lastday:'.$month_lastday->format('Y-m-d'));
 		}
 
-		// if(!class_exists('SoapOAuthWrapper')){
-		// 	return 'SoapOAuthWrapper doesn\'t exist';
-		// }
-
 		$options = get_option('tmsm-availpro-options', false);
 		$rateids = $options['accommodationrateids'];
 
 		$soap_parameters = array(
 			'groupId'   => $options['groupid'],
 			'hotelId'   => $options['hotelid'],
-			// 'beginDate' => "2024-09-01",
-			// 'endDate'   => "2024-09-30",
 			'beginDate' => $month_firstday->format('Y-m-d'),
 			'endDate'   => $month_lastday->format('Y-m-d'),
 			'layout'    => $this->get_layout(),
-			// 'filter'    => json_encode($this->get_filters($rateids)),
 			'filter'    => $this->get_filters($rateids),
 		);
 		try {
@@ -262,47 +181,18 @@ class Tmsm_Availpro_Webservice {
 					'body' => json_encode($soap_parameters),
 			);
 			$response = wp_remote_post( self::URL.'GetDailyPlanning', $options );
-				//TODO traiter les différentes erreurs de retour 200 300 400
-				error_log(print_r(json_decode($response['body']), true));
-			
+			if($response['response']['code'] == 200)
+			{
 				return json_decode($response['body'], true);
-			if ($response === FALSE) {
-				echo "Error: " . error_get_last()['message'] . "\n";
-			} else {
-				error_log('TMSM_SIGNATURE');
-				error_log( print_r(json_decode($oauth_signature), true));
-				
+			} else 
+			{
+				echo $response['response']['message'];
 			}
-			die;
+
 		} catch ( OAuthException2 $e ) {
 			return $e;
 		}
 	}
-// TODO remove the function no more needed
-	// /**
-	//  * Convert XML results in array
-	//  *
-	//  * @param string $xml
-	//  *
-	//  * @return array
-	//  */
-	// static public function convert_to_array($xml){
-
-	// 	$domObject = new DOMDocument();
-	// 	$domObject->loadXML($xml);
-
-	// 	$domXPATH = new DOMXPath($domObject);
-	// 	$results = $domXPATH->query("//soap:Body/*");
-
-	// 	$array = [];
-	// 	foreach($results as $result)
-	// 	{
-	// 		$array = json_decode(json_encode(simplexml_load_string($result->ownerDocument->saveXML($result))), true);
-	// 	}
-	// 	return $array;
-	// }
-
-
 
 	/**
 	 * Get Stay Planning from Availpro API call
@@ -349,9 +239,12 @@ class Tmsm_Availpro_Webservice {
 					'body' => json_encode($soap_parameters),
 			);
 			$response = wp_remote_post( self::URL.'GetStayPlanning', $options );
-			//TODO gérer une réponse erreur
 			if($response['response']['code'] === 200 ){
 				return json_decode($response['body'], true);
+			}
+			else 
+			{
+				echo $response['response']['message'];
 			}
 		} catch ( OAuthException2 $e ) {
 			return $e;
